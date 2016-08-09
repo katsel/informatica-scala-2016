9a97fed24e10ddd32ff525cfec741256a8e6de92
katsel <katsel@users.noreply.github.com>
2016-08-09 15:47:02 +0200

    Course block day 2, part 2: Work on the bot

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -9,7 +9,16 @@ class ControlFunction {
     val tokens: Array[String] = input.split('(')
     if (tokens(0)=="React") /* e.g. React(generation=0,time=0,view=__W_W_W__,energy=100) */ {
       val params: Array[String] = tokens(1).dropRight(1).split(',')
-      val energy: String = params(3).split('=')(1)
+
+      // store the parameters as key-value pairs as a map
+      // step 1: String to Array
+      val keyValueArrays: Array[Array[String]] = params.map(param => param.split('='))
+      // step 2: Array to Tuples
+      val keyValueTuples: Array[(String, String)] = keyValueArrays.map(a => (a(0), a(1)))
+      // step 3: Tuples to Map
+      val paramMap: Map[String, String] = keyValueTuples.toMap
+
+      val energy: String = paramMap("energy")
       s"Move(direction=0:-1)|Status(text=$energy)"
     } else ""
   }
